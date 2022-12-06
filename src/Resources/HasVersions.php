110522d69db36f591d69cea029f8826de074d812
Matthew Poulter <poulter@vcsw.nl>
2022-12-06 15:37:33 +0100

    Complete cleanup

--- src/Resources/HasVersions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SimpleSquid\Vend\Resources;
-
-trait HasVersions
-{
-    /** @var \SimpleSquid\Vend\Resources\TwoDotZero\Version|null */
-    public $version;
-}
---
+++ tests/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace SimpleSquid\Vend\Tests;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
