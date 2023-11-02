0dfac465b234d5490432d1920706e81c6a88b667
Matthew Poulter <poulter@vcsw.nl>
2023-11-02 08:00:41 +0100

    Simplify connector

--- src/VendConnector.php
+++ src/VendConnector.php
@@ -3,11 +3,11 @@
 namespace SimpleSquid\Vend;
 
 use Composer\InstalledVersions;
+use Saloon\Contracts\Authenticator;
 use Saloon\Helpers\OAuth2\OAuthConfig;
 use Saloon\Http\Connector;
 use Saloon\Traits\OAuth2\AuthorizationCodeGrant;
 use Saloon\Traits\Plugins\AcceptsJson;
-use SimpleSquid\Vend\Common\Exceptions\AuthenticationDetailsMissingException;
 use SimpleSquid\Vend\Common\Exceptions\DomainPrefixMissingException;
 use SimpleSquid\Vend\Common\Responses\VendResponse;
 
@@ -18,29 +18,26 @@ abstract class VendConnector extends Connector
 
     protected ?string $response = VendResponse::class;
 
+    protected ?string $domainPrefix;
+
+    /**
+     * @var callable(): ?\Saloon\Contracts\Authenticator
+     */
+    protected $defaultAuth;
+
     public function __construct(
         ?string $clientId = null,
         ?string $clientSecret = null,
         ?string $redirectUri = null,
-        ?string $personalToken = null,
-        protected ?string $domainPrefix = null,
     ) {
-        if (! is_null($personalToken)) {
-            $this->withTokenAuth($personalToken);
-
+        if (is_null($clientId) || is_null($clientSecret) || is_null($redirectUri)) {
             return;
         }
 
-        if (! is_null($clientId) && ! is_null($clientSecret) && ! is_null($redirectUri)) {
-            $this->oauthConfig()
-                ->setClientId($clientId)
-                ->setClientSecret($clientSecret)
-                ->setRedirectUri($redirectUri);
-
-            return;
-        }
-
-        throw new AuthenticationDetailsMissingException();
+        $this->oauthConfig()
+            ->setClientId($clientId)
+            ->setClientSecret($clientSecret)
+            ->setRedirectUri($redirectUri);
     }
 
     public function resolveBaseUrl(): string
@@ -59,6 +56,14 @@ abstract class VendConnector extends Connector
         return $this;
     }
 
+    /**
+     * @param  callable(): ?\Saloon\Contracts\Authenticator  $callable
+     */
+    public function authenticateWith(callable $callable): static
+    {
+        $this->defaultAuth = $callable;
+    }
+
     protected function defaultHeaders(): array
     {
         return [
@@ -72,5 +77,10 @@ abstract class VendConnector extends Connector
             ->setAuthorizeEndpoint('https://secure.vendhq.com/connect')
             ->setTokenEndpoint('/1.0/token')
             ->setUserEndpoint('/2.0/retailer');
+    }
+
+    protected function defaultAuth(): ?Authenticator
+    {
+        return isset($this->defaultAuth) ? ($this->defaultAuth)() : null;
     }
 }
